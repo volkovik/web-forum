93e67c8566b3aa829b4564e7b05311bb003b979a
Daniil Surmachenko <dansur4ik@mail.ru>
2021-03-28 15:17:15 +0300

    Добавлена модель пользователя

--- database/models.py
+++ database/models.py
@@ -1,4 +1,16 @@
+import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+
 from .session import SqlAlchemyBase
 
 
 # Модели базы данных
+class User(SqlAlchemyBase):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_time = Column(DateTime, default=datetime.datetime.now)
